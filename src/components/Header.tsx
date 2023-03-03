0af53b6e241c6fe66d6265ac19fc18539c2441d8
timchee <shpejtim.ukaj@life.gjirafa.com>
2023-03-03 13:37:05 +0100

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+export function Headers(){
+    return (
+    <div className="header">
+        <div className="header__description">
+            <h1>Technical Documentation</h1>
+            <p>Browse the latest developer documentation including tutorials,
+                sample code, articles and API references.
+            </p>
+            <button>Get Started</button>
+        </div>
+        <div className="header__logo">
+            <div className="header__logo--shadow">
+                <h1>&alefsym;</h1>
+            </div>
+            <h1>&alefsym;</h1>
+        </div>
+    </div>
+    )
+}
+
