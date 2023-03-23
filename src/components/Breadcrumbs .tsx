f167daf13593238c9c536e780a2bbdff0fe1b192
Arbrorë Halilaj <arbrore@gjirafa.com>
2023-03-23 11:48:29 +0100

    Added the left side menu headings

--- src/components/Breadcrumbs .tsx
+++ src/components/Breadcrumbs .tsx
@@ -1,11 +1,24 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
+import { useQuery, gql } from "@apollo/client";
+import { ARTICLES_QUERY } from "../../graphql/articles";
+import { Article } from "../../interfaces/IMain";
 
-export default function Breadcrumbs () {
+export default function Breadcrumbs() {
+    const router = useRouter();
+    const { loading, error, data } = useQuery(ARTICLES_QUERY);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+  
+    // Find the article with the matching ID
+    const currentArticle = data.codexguidearticlesCollection.items.find(
+      (article: Article) => article.id === router.query.id
+    );
+  
     return (
-        <>
-        <div className="breadcrumbs">
-            <span>Welcome</span> <span>&#10097;</span> <span>What is Codex</span> <span>&#10097;</span> <span>Started</span>
-        </div>
-        </>
-    )
+      <div className="breadcrumbs">
+        <span>User Guide</span> <span>&#10097;</span>{" "}
+        {currentArticle && <span>{currentArticle.title}</span>}
+      </div>
+    );
 }
--- src/components/RightSideMenu.tsx
+++ src/components/RightSideMenu.tsx
@@ -1,23 +1,46 @@
+import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
 import Link from "next/link";
+import { useEffect } from "react";
+import { ARTICLES_QUERY } from "../../graphql/articles";
+import { Article } from "../../interfaces/IMain";
 
 export default function RightSideMenu() {
+    const router = useRouter();
+    
+    const { loading, error, data, refetch } = useQuery(ARTICLES_QUERY);
+
+    useEffect(() => {
+      if (!loading) {
+        console.log("Data loaded successfully", data);
+      }
+    }, [loading]);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    // Find the article with the matching ID
+    const currentArticle = data.codexguidearticlesCollection.items.find(
+        (article: Article) => article.id === router.query.id
+    );
+
+    if (!currentArticle) {
+        return <p></p>;
+    }
+
+    // Filter the articleBody array to only include objects with type equal to "heading"
+    const headings = currentArticle.articleBody.filter(
+        (block: any) => block.type === "heading"
+    );
+
     return (
         <section className="right--section">
             <div className="right--section__content">
                 <h6>Currently Viewing</h6>
                 <ul>
-                    <li>
-                        <a>What is Codex</a>
-                    </li>
-                    <li>
-                        <a>How to get started</a>
-                    </li>
-                    <li>
-                        <a>Create your first organization</a>
-                    </li>
-                    <li>
-                        <a>Create your first page</a>
-                    </li>
+                {headings.map((heading: any, index: number) => (
+                    <li key={index} dangerouslySetInnerHTML={{ __html: heading.contentHTML }} />
+                ))}
                 </ul>
             </div>
         </section>
